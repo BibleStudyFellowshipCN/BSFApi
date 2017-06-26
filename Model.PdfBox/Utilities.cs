f1e1bc5b405c93cde601e70534fbfb4b949859cc
Rui Min <rui_m99@hotmail.com>
2017-06-26 00:20:59 -0700

    PDFBox library.

---
+++ Model.PdfBox/Utilities.cs
@@ -0,0 +1,24 @@
+﻿namespace Church.BibleStudyFellowship.Models.PdfBox
+{
+    using org.apache.pdfbox.pdmodel;
+    using org.apache.pdfbox.util;
+
+    public static partial class Utilities
+    {
+        public static string ReadFromPdf(string filename)
+        {
+            PDDocument doc = null;
+
+            try
+            {
+                doc = PDDocument.load(filename);
+                var stripper = new PDFTextStripper();
+                return stripper.getText(doc);
+            }
+            finally
+            {
+                doc?.close();
+            }
+        }
+    }
+}
