2569bf19e1ed42de3a505e37d03e4b99f40b767e
Rui Min <rui_m99@hotmail.com>
2018-08-26 21:34:17 -0700

    Update chapter verse pattern for Chinese

--- Models/VerseLocatorZh.cs
+++ Models/VerseLocatorZh.cs
@@ -2,7 +2,7 @@
 {
     internal class VerseLocatorZh : VerseLocator
     {
-        const string ChapterPattern = @"第 *([一二三四五六七八九十百]+|\d+) *章(和第 *([一二三四五六七八九十百]+|\d+) *章)?";
+        const string ChapterPattern = @"第 *([一二三四五六七八九十百]+|\d+) *([章篇]?[、和-]第? *([一二三四五六七八九十百]+|\d+) *)*[章篇]";
 
         internal VerseLocatorZh(string books)
             : base(books, ":：", "-", ",，、", ";；", VerseLocatorZh.ChapterPattern)
